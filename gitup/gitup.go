0cae3015513e1273ac9453b2f1c883b1c71eefe0
BudacaB <budaca.b@gmail.com>
2019-10-17 20:27:26 +0300

    testing

--- gitup/gitup.go
+++ gitup/gitup.go
@@ -5,17 +5,25 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 	"time"
 )
 
 func execute() {
 
+	var executeWith string
 	var commitMessage string
 	var branchName string
 
 	flag.StringVar(&commitMessage, "m", "", "Input commit message (Required)")
 	flag.StringVar(&branchName, "b", "", "Input branch name - master or other (Required)")
 	flag.Parse()
+
+	if runtime.GOOS == "windows" {
+		executeWith = "powershell"
+	} else if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		executeWith = "bash"
+	}
 
 	if commitMessage == "" {
 		flag.PrintDefaults()
@@ -27,25 +35,25 @@ func execute() {
 		os.Exit(1)
 	}
 
-	outStatus, err := exec.Command("powershell", "git status").Output()
+	outStatus, err := exec.Command(executeWith, "git status").Output()
 	outputStatus := string(outStatus[:])
 	fmt.Println(outputStatus)
 
 	time.Sleep(3 * time.Second)
 
-	outAdd, err := exec.Command("powershell", "git add .").Output()
+	outAdd, err := exec.Command(executeWith, "git add .").Output()
 	outputAdd := string(outAdd[:])
 	fmt.Println(outputAdd)
 
 	time.Sleep(5 * time.Second)
 
-	outCommit, err := exec.Command("powershell", fmt.Sprintf("git commit -am \"%s\"", commitMessage)).Output()
+	outCommit, err := exec.Command(executeWith, fmt.Sprintf("git commit -am \"%s\"", commitMessage)).Output()
 	outputCommit := string(outCommit[:])
 	fmt.Println(outputCommit)
 
 	time.Sleep(5 * time.Second)
 
-	outPush, err := exec.Command("powershell", fmt.Sprintf("git push origin \"%s\"", branchName)).Output()
+	outPush, err := exec.Command(executeWith, fmt.Sprintf("git push origin \"%s\"", branchName)).Output()
 	outputPush := string(outPush[:])
 	fmt.Println(outputPush)
 
